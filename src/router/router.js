bbb429ab04eed3fb5ad8e946080090ae921c0094
Va54 <hayrapetyan.vardank@gmail.com>
2019-01-25 19:41:18 +0400

    Remove redundant components

--- src/router/router.js
+++ src/router/router.js
@@ -1,25 +1,17 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from '../views/Home.vue';
-
+// example for importing component with promise
+// const main = () => import(path);
 Vue.use(Router);
 
 export default new Router({
   mode: 'history',
-  base: process.env.BASE_URL,
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-    },
+    // {
+    //   path: '/',
+    //   name: '',
+    //   component: ,
+    // },
+
   ],
 });
