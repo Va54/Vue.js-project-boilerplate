6bdec7254d9020f0b2e5658386dbdc459be04d4f
Va54 <hayrapetyan.vardank@gmail.com>
2019-02-21 22:01:50 +0400

    Add configs for webpack build

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,44 @@
-module.exports = {
+const webpack = require('webpack');
 
+module.exports = {
+  chainWebpack: (config) => {
+    config.module
+      .rule('images')
+      .test(/\.(a?png|png|jpe?g|gif|webp)(\?.*)?$/)
+      .use('url-loader')
+      .loader('url-loader')
+      .tap(options => options);
+    config.plugins.delete('prefetch');
+  },
+  configureWebpack: {
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+        minSize: 10000,
+        maxSize: 250000,
+      },
+    },
+    plugins: [
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^\.\/locale$/,
+        contextRegExp: /moment$/,
+      }),
+    ],
+  },
+  css: {
+    loaderOptions: {
+      sass: {
+        data: `
+        @import "@/assets/styles/utils/_variables.scss";
+        @import "@/assets/styles/utils/_mixins.scss";
+        @import "@/assets/styles/utils/_buttons.scss";
+        @import "@/assets/styles/utils/_baseInput.scss";
+        @import "@/assets/styles/utils/_baseCheckbox.scss";
+        @import "@/assets/styles/utils/_baseDropdown.scss";
+        `,
+      },
+    },
+  },
+  lintOnSave: false,
 };
+
